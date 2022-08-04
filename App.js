14238559dd8ea96aa3f525b6dfab12b8079f4136
IDB_heni@idealbloom.io <yjshim>
2022-08-04 19:13:51 +0900

    feat: AppLoading 1 font, image&asset

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from "react";
 import AppLoading from "expo-app-loading";
+import React from "react";
+import { Text, Image } from "react-native";
 import { useState } from "react/cjs/react.development";
-import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+
+import * as Font from "expo-font";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
     //await 해야되는 모든 로딩을 담당하는 곳
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    await Font.loadAsync(Ionicons.font);
+    // await Asset.loadAsync(require("url"));r
+
+    /*
+      let [fontsLoaded] = useFonts({
+        'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+      });
+    */
   };
   if (!ready) {
     return (
