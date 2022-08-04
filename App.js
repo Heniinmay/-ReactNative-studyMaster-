e27736b5beacdfdca7eb2e1f417588c81280f312
IDB_heni@idealbloom.io <yjshim>
2022-08-04 19:31:19 +0900

    feat: AppLoading 2 font, asset image mapping

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import AppLoading from "expo-app-loading";
-import React from "react";
-import { Text, Image } from "react-native";
-import { useState } from "react/cjs/react.development";
 import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
-
 import * as Font from "expo-font";
+import React from "react";
+import { Image, Text } from "react-native";
+import { useState } from "react/cjs/react.development";
+
+// 배열 fonts를 loadFonts 함수에 주면 Font.loadAsync(font)로 이루어진 배열을 리턴 받을 거야
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (Images) =>
+  Images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
     //await 해야되는 모든 로딩을 담당하는 곳
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    await Font.loadAsync(Ionicons.font);
-    // await Asset.loadAsync(require("url"));r
-
-    /*
-      let [fontsLoaded] = useFonts({
-        'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
-      });
-    */
+    const fonts = loadFonts([Ionicons.font]);
+    // const images = loadImages([require("./assets/image.png"),"https://url.com"]); // image 배열
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
