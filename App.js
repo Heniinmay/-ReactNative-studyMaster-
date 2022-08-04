2376aa87befafdad0be79dd10e83121b560825ce
IDB_heni@idealbloom.io <yjshim>
2022-08-04 18:44:18 +0900

    feat: AppLoading 1 setting, props

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
+import { useState } from "react/cjs/react.development";
+import { Text } from "react-native";
 
 export default function App() {
-  return null;
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    //await 해야되는 모든 로딩을 담당하는 곳
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading} // 끝나면 finish 함수 호출
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+  return <Text>hello, world</Text>;
 }
